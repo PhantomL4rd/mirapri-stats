f26ba35216ad9d3d73d1de33111bd7cd063e322c
PhantomL4rd <233923175+PhantomL4rd@users.noreply.github.com>
2026-01-25 18:34:49 +0900

    ogp

--- apps/web/astro.config.ts
+++ apps/web/astro.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  site: 'https://mirapri-insight.pl4rd.com',
   output: 'server',
   adapter: cloudflare({
     imageService: 'compile',
